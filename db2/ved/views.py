ce774224bf43a1b97926f507ae63ae335dfb1126
das <sudmal@gmail.com>
2020-09-16 17:36:38 +0300

    ind_report_view1

--- db2/ved/views.py
+++ db2/ved/views.py
@@ -6,6 +6,8 @@ from .models import Organisation
 from .forms import SearchForm
 
 
+from .models import Records, Organisation, Organization, Trademark
+
 # Create your views here.
 def index(request):
     return render(request,'ved/index.html')
@@ -35,6 +37,10 @@ def IndividualReportFirmDetail(request):
     if request.method == 'POST':
         edrpou_detail = request.POST['edrpou_detail']
         context = {"edrpou_detail": edrpou_detail}
+        queryset_list = Records.objects.filter(recipient__edrpou__icontains=organization)\
+                .values('gtd__date', 'gtd__name', 'recipient__name', 'recipient__edrpou').order_by('-gtd__date')\
+                .annotate(gtd_count=Count('gtd__name'), total_cost=Sum('gtd__cost_fact'), tms=ArrayAgg('gtd__trademark__name', distinct=True))
+
         return render(request,'ved/IndividualReportFirmDetail.html',context)
     else:
         return(HttpResponseRedirect(reverse('IndividualReport')))
