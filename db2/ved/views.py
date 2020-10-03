9d6b554808b863a6bdeb76e443f494574714b9c0
das <sudmal@gmail.com>
2020-10-03 18:03:52 +0300

    Make GET requests in IndRep-1 an make paginatipns in IR-1 and IR3

--- db2/ved/views.py
+++ db2/ved/views.py
@@ -28,8 +28,7 @@ def CompetitorsComparse(request):
 
 def test(request):
     gtdrecords = GtdRecords.objects.filter(trademark__name__icontains='UNOX')
-    paginator = Paginator(gtdrecords, 5) # Show 25 contacts per page.
-
+    paginator = Paginator(gtdrecords, 10)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     context = {
@@ -38,26 +37,28 @@ def test(request):
     return render(request,'ved/test.html',context)
 
 def IndividualReport(request):
-    competitors = Competitors.objects.all()
-    search_form = SearchForm()
     context=dict()
- 
-    # if this is a POST request we need to process the form data
-    if request.method == 'POST':
+    search_form = SearchForm()
 
-        grecords = GtdRecords.objects.filter((Q(record__recipient__edrpou__startswith=request.POST['search_string']) | \
-             Q(record__recipient__name__icontains=request.POST['search_string'])) & Q(record__date__range=[request.POST["start_date"], request.POST["end_date"]]))\
+    if request.GET.get('search_string'):
+        search_form = SearchForm(request.GET)
+        grecords_all = GtdRecords.objects.filter((Q(record__recipient__edrpou__startswith=request.GET.get('search_string')) | \
+             Q(record__recipient__name__icontains=request.GET.get('search_string'))) & Q(record__date__range=[request.GET.get('start_date'), request.GET.get('end_date')]))\
                 .values('record__recipient__edrpou','record__recipient__name','record__recipient__is_competitor')\
                  .annotate(count=Count("cost_fact"),total_cost=Sum('cost_fact'), tms_count=Count('trademark__name',distinct=True),\
                      tms=ArrayAgg('trademark__name', distinct=True)).order_by('-total_cost')
-        print (str(len(grecords)))
-        search_form = SearchForm(request.POST)
+        paginator = Paginator(grecords_all, 10)
+        page_number = request.GET.get('page')
+        grecords = paginator.get_page(page_number)
+        print(page_number)
         context = {
-            "grecords": grecords,
-            "start_date": request.POST["start_date"],
-            "end_date": request.POST["end_date"]
+            "search_string": request.GET.get('search_string'),
+            "pages": grecords,
+            "grecords": grecords.object_list,
+            "start_date": request.GET.get('start_date'),
+            "end_date": request.GET.get('end_date')
             }
-    context.update({"search_form": search_form, 'competitors': competitors})
+    context.update({"search_form": search_form})
     return render(request,'ved/IndividualReport.html',context)
 
 def IndividualReportFirmShow(request,edrpou_num):
@@ -85,10 +86,12 @@ def IndividualReportRaw(request,edrpou_num,gtd_num):
     queryset_list = GtdRecords.objects.filter((Q(record__recipient__edrpou=edrpou_num) & Q(record__gtd_name=gtd)))\
             .values('record__sender__name','record__sender__country__name','record__date','product_code','trademark__name','description','cost_fact').order_by('record__date')
     print(queryset_list.query)
+    paginator = Paginator(queryset_list, 10)
+    page_number = request.GET.get('page')
+    records = paginator.get_page(page_number)
     context = {
-                "records":queryset_list,
+                "records":records,
                 "gtd": gtd,
                 'edrpou_num':edrpou_num,
             }
-            
     return render(request,'ved/IndividualReportRaw.html',context)
