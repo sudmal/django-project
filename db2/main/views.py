5ea835bff61c87bc129ba94976e9d236cfb70c17
das <sudmal@gmail.com>
2020-10-03 23:06:52 +0300

    fix import

--- db2/main/views.py
+++ db2/main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
-#from django.contrib.admin import login,authenticate
+from django.contrib.auth import login,authenticate
 from django.urls import reverse
 
 # Create your views here.
