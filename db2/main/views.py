a9662cffa92bfaeb5570d4648cc85b86cc197eef
das <sudmal@gmail.com>
2020-10-03 22:58:26 +0300

    sutting up login view

--- db2/main/views.py
+++ db2/main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.http import HttpResponse, HttpResponseRedirect
+#from django.contrib.admin import login,authenticate
+from django.urls import reverse
 
 # Create your views here.
 def index(request):
