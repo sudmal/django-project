40c85ab54ba4d2cf96c5ff3c57f1a1c50ed6d4f6
sudmal <sudmal@gmail.com>
2020-10-22 01:55:52 +0300

    some changes

--- db2/ved/forms.py
+++ db2/ved/forms.py
@@ -7,11 +7,11 @@ class DateInput(forms.DateInput):
     input_type='date'
 
 class SearchForm(forms.Form):
-    search_string = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control','width':'500px','autofocus':'yes','placeholder': 'Введите текст','id':'tm_search'}), max_length=100)
+    search_string = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control','width':'500px','autofocus':'yes','placeholder': 'Введите текст','id':'tm_search'}), max_length=255)
     start_date = forms.DateField(input_formats='%Y,%m,%d',widget=DateInput(attrs={'class': 'form-control date-inline-select','value':year+'-01-01'}))
     end_date = forms.DateField(input_formats='%Y,%m,%d',widget=DateInput(attrs={'class': 'form-control date-inline-select','value':year+'-12-31'}))
 class SearchFormOrg(forms.Form):
-    search_string = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control','width':'500px','autofocus':'yes','placeholder': 'Введите наименование или ЕДРПОУ','id':'org_search'}), max_length=100)
+    search_string = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control','autofocus':'yes','placeholder': 'Введите наименование или ЕДРПОУ','id':'org_search'}), max_length=255)
     start_date = forms.DateField(input_formats='%Y,%m,%d',widget=DateInput(attrs={'class': 'form-control date-inline-select','value':year+'-01-01'}))
     end_date = forms.DateField(input_formats='%Y,%m,%d',widget=DateInput(attrs={'class': 'form-control date-inline-select','value':year+'-12-31'}))
  
