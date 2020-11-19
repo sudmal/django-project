40c612ba96a01c72a62c97c962a9b6d857e12a44
das <sudmal@gmail.com>
2020-11-19 18:10:43 +0300

    fix profiles

--- db2/main/models.py
+++ db2/main/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-class Profile(models.Model):
+'''class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(max_length=500, blank=True)
     currency = models.CharField(max_length=30, blank=True)
@@ -22,3 +22,4 @@ def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
 
 # https://metanit.com/python/django/5.8.php
+'''
