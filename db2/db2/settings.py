684a04af62a7fcfc803adac4e0f77f6a4f6d2b1e
das <sudmal@gmail.com>
2021-02-21 20:29:52 +0300

    SOME_FIXES

--- db2/db2/settings.py
+++ db2/db2/settings.py
@@ -173,8 +173,8 @@ STATIC_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 DATE_FORMAT='Y-m-d'
-#FILEBROWSER_DIRECTORY = ''
-#DIRECTORY = ''
+FILEBROWSER_DIRECTORY = ''
+DIRECTORY = ''
 
 
 TINYMCE_JS_URL = os.path.join(STATIC_URL, "django_tinymce/tinymce.min.js")
