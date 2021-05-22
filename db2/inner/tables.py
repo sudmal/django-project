0e8d875bd114dbdf27395e722a35e36b894bc9a1
das <sudmal@gmail.com>
2021-05-22 23:14:56 +0300

    Tuning RecordsSearchTable

--- db2/inner/tables.py
+++ db2/inner/tables.py
@@ -6,6 +6,7 @@ from .models import NlReestr
 class RecordsSearchTable(tables.Table):
     export_formats = ['csv', 'xls', 'xlsx']
     num = tables.TemplateColumn("{{ row_counter }}",verbose_name= '№' )
+    curr = tables.TemplateColumn("{{ currency }}",verbose_name= 'Валюта' )
     #_name = columns.Column(visible=False,exclude_from_export=True)
     #recipient_code = columns.Column(visible=False,exclude_from_export=True)
     #hash = columns.Column(visible=False,exclude_from_export=True)
@@ -16,7 +17,7 @@ class RecordsSearchTable(tables.Table):
     seller__edrpou = tables.Column(verbose_name= 'ЕДРПОУ продавца' )
     buyer__name = tables.Column(verbose_name= 'Покупатель' )
     buyer__edrpou = tables.Column(verbose_name= 'ЕДРПОУ покупателя' )
-    product__product_code = tables.Column(verbose_name= 'Товарный номер' )
+    product__product_code = tables.Column(verbose_name= 'Код УКТВЭД' )
     product__name = tables.Column(verbose_name= 'Наименование товара' )
     cost = tables.Column(verbose_name= 'Цена' )
     count = tables.Column(verbose_name= 'Количество' )
@@ -25,7 +26,7 @@ class RecordsSearchTable(tables.Table):
     class Meta:
         export_formats = ['csv', 'xls', 'xlsx']
         model = NlReestr
-        fields = ('num','ordering_date','seller__name','seller__edrpou','buyer__name','buyer__edrpou','product__product_code','product__name','cost','count', 'total_cost')
-        sequence = ('num','ordering_date','seller__name','seller__edrpou','buyer__name','buyer__edrpou','product__product_code','product__name','cost','count', 'total_cost')
+        fields = ('num','ordering_date','seller__name','seller__edrpou','buyer__name','buyer__edrpou','product__product_code','product__name','cost','count', 'total_cost', 'curr')
+        sequence = ('num','ordering_date','seller__name','seller__edrpou','buyer__name','buyer__edrpou','product__product_code','product__name','cost','count', 'total_cost', 'curr')
         # add class="paleblue" to <table> tag
         attrs = {'class': 'paleblue'}
