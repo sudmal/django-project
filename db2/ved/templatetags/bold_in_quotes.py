8a1c9675d1786c15aeccd560b2e96febf59d0fd1
das <sudmal@gmail.com>
2021-05-25 22:41:25 +0300

    upgrade nold_in_quotes

--- db2/ved/templatetags/bold_in_quotes.py
+++ db2/ved/templatetags/bold_in_quotes.py
@@ -6,6 +6,7 @@ def bold_in_quotes(str1):
     st=''
     result=''
     
+    # Заменяем все возможные варианты кавычек на простые двойные
     if str1.find("«")>=0 and str1.find("»")>=0:
         str1=str1.replace("«","\"")
         str1=str1.replace("»","\"")
@@ -14,14 +15,21 @@ def bold_in_quotes(str1):
         str1=str1.replace("“","\"")
     if str1.find("'")>=0:
         str1=str1.replace("'","\"")
+
+
     if str1.find("\"")>=0 :
         st=str1.split("\"")
-        result="<small class=\"text-muted\">"+st[0]+"</small><br><span class=\"font-weight-bold\">\""
-        for s in st[1:]:
-            result=result+s
-        result=result+"\"</span>"    
+
+        if len(st[0])>0:
+            result='<span style="cursor: help; font-size: 20px !important; font-weight: bold;" data-toggle="tooltip" data-placement="right" title="'+st[0]+'">'
+    #        result="<small class=\"text-muted\">"+st[0]+"</small><br><span class=\"font-weight-bold\">"
+            for s in st[1:]:
+                result=result+s
+            result=result+"</span>"
+        else:
+            result="<span style=\"font-size: 20px !important; font-weight: bold;\">"+str1+"</span>"     
     else:
-        result="<span class=\"font-weight-bold\">\""+str1+"\"</span>"
+        result="<span style=\"font-size: 20px !important; font-weight: bold;\">"+str1+"</span>"
     return(result)
 
 
