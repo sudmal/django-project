bfc92bcf8ab0e0d4ebab799e245d60a54e215fe6
das <sudmal@gmail.com>
2021-06-17 00:16:29 +0300

    ved competitors catalog raw search rus columns

--- db2/ved/tables.py
+++ db2/ved/tables.py
@@ -9,6 +9,15 @@ class CompetitorsComparsePeriodDetailTable(tables.Table):
 
     recipient_name = columns.Column(visible=False,exclude_from_export=True)
     recipient_code = columns.Column(visible=False,exclude_from_export=True)
+    date = tables.Column(verbose_name= 'Дата')
+    gtd = tables.Column(verbose_name= 'ГТД')
+    country = tables.Column(verbose_name= 'Страна')
+    sender_name = tables.Column(verbose_name= 'Отправитель')
+    product_code = tables.Column(verbose_name= 'ТНВЭД')
+    trademark = tables.Column(verbose_name= 'Торговая марка')
+    description = tables.Column(verbose_name= 'Описание')
+    cost_fact = tables.Column(verbose_name= 'Фактурная стоимость')
+    cost_customs = tables.Column(verbose_name= 'Таможенная стоимость')
     hash = columns.Column(visible=False,exclude_from_export=True)
     #rename columns
     # devenv = tables.Column(verbose_name= 'Development Environment' )
