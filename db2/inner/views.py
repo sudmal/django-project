6b182ce430d632f1a5245d749a1b973b072896e1
Dmitriy <sudmal@gmail.com>
2021-08-19 00:02:34 +0300

    topSales - urls-templates

--- db2/inner/views.py
+++ db2/inner/views.py
@@ -1025,40 +1025,12 @@ def topSales(request):
     if request.GET.get('search_string'):
         recSearchForm = RecSearchForm(request.GET)
 
-        results=NlReestr.objects.filter(Q(seller__name__icontains=request.GET.get('search_string')) |Q(seller__edrpou__startswith=request.GET.get('search_string')) | \
-            Q(buyer__name__icontains=request.GET.get('search_string')) |Q(buyer__edrpou__startswith=request.GET.get('search_string')) | \
-                Q(product__name__icontains=request.GET.get('search_string')) |Q(product__product_code__startswith=request.GET.get('search_string')) )\
-                    .values('ordering_date','seller__name','buyer__name','seller__edrpou','buyer__edrpou','product__product_code','product__name','count')
-        print(results.query)
-        if currency == 'UAH':
-            results=results.annotate(total_cost=Round2(Sum(F('one_product_cost')*F('count')+F('one_product_cost')*F('count')*0.2)),cost=Round2(F('one_product_cost')+F('one_product_cost')*0.2)).order_by('ordering_date')
-        elif currency == 'EUR':
-            results=results.annotate(total_cost=Round2(Sum((F('one_product_cost')*F('count')+F('one_product_cost')*F('count')*0.2)/F('exchange__eur_mb_sale'))),cost=Round2((F('one_product_cost')+F('one_product_cost')*0.2)/F('exchange__eur_mb_sale'))).order_by('ordering_date')
-        elif currency == 'USD':
-            results=results.annotate(total_cost=Round2(Sum((F('one_product_cost')*F('count')+F('one_product_cost')*F('count')*0.2)/F('exchange__usd_com'))),cost=Round2((F('one_product_cost')+F('one_product_cost')*0.2)/F('exchange__usd_com'))).order_by('ordering_date')
- 
-    total_sum=0
-
-    for product in results:
-        if product['total_cost'] != None:
-            total_sum+=product['total_cost']
-    total_sum=round(total_sum)
-
-    table = RecordsSearchTable(results)
-    RequestConfig(request, paginate={"per_page": num_per_page}).configure(table)
-    if request.GET.get('_export'):
-        name=request.GET.get('search_string')
-        export_format = request.GET.get("_export", None)
-        if TableExport.is_valid_format(export_format):
-            exporter = TableExport(export_format, table, dataset_kwargs={"title": 'Db_search'})
-            return exporter.response(filename='NalogSalesSearch.{0}'.format(export_format)) #
+       
     context={
         'recSearchForm':recSearchForm,
-        'total_sum':total_sum,
-        'table':table,
         'results':results,
         'currency':currency,
         }
-    context.update({'help_page_id':13})
-    return render(request,'inner/RecordsSearch.html',context)
+    context.update({'help_page_id':19})
+    return render(request,'inner/topSales.html',context)
 
