77882ad0d59c5591ab9618d8df5975a52ef3cfd0
Dmitriy <sudmal@gmail.com>
2021-09-01 18:32:01 +0300

    bold_in_quotes_font_16

--- db2/ved/templatetags/bold_in_quotes.py
+++ db2/ved/templatetags/bold_in_quotes.py
@@ -21,15 +21,15 @@ def bold_in_quotes(str1):
         st=str1.split("\"")
 
         if len(st[0])>0:
-            result='<span style="cursor: help; font-size: 20px !important; font-weight: bold;" data-toggle="tooltip" data-placement="right" title="'+st[0]+'">'
+            result='<span style="cursor: help; font-size: 16px !important; font-weight: bold;" data-toggle="tooltip" data-placement="right" title="'+st[0]+'">'
     #        result="<small class=\"text-muted\">"+st[0]+"</small><br><span class=\"font-weight-bold\">"
             for s in st[1:]:
                 result=result+s
             result=result+"</span>"
         else:
-            result="<span style=\"font-size: 20px !important; font-weight: bold;\">"+str1+"</span>"     
+            result="<span style=\"font-size: 16px !important; font-weight: bold;\">"+str1+"</span>"     
     else:
-        result="<span style=\"font-size: 20px !important; font-weight: bold;\">"+str1+"</span>"
+        result="<span style=\"font-size: 16px !important; font-weight: bold;\">"+str1+"</span>"
     return(result)
 
 
