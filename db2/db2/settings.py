0ad22f781fde8739b70eb7f39a3dcd17e6fdb184
Dmitriy <sudmal@gmail.com>
2021-10-22 21:56:51 +0300

    HTTPS_setup

--- db2/db2/settings.py
+++ db2/db2/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
+    'django_extensions',
     'tinymce',
     'main',
     'ved',
